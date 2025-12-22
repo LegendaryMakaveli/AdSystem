bc349d286e94dc338f36417cb91d0115cba83666
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 20:33:34 +0100

    app mapper

--- src/main/java/com/adSystems/util/Mapper.java
+++ src/main/java/com/adSystems/util/Mapper.java
@@ -1,4 +1,35 @@
 package com.adSystems.util;
 
+import com.adSystems.datas.models.Listing;
+import com.adSystems.datas.models.ListingStatus;
+import com.adSystems.dtos.reponses.ListingResponse;
+import com.adSystems.dtos.requests.ListingRequests;
+
+import java.time.LocalDateTime;
+
 public class Mapper {
+
+    public static Listing mapToCreateAListing(ListingRequests request){
+        Listing listing = new Listing();
+        listing.setTitle(request.getTitle());
+        listing.setDescription(request.getDescription());
+        listing.setPrice(request.getPrice());
+        listing.setEmail(request.getEmail());
+        listing.setCityId(request.getCityId());
+        listing.setCategoryId(request.getCategoryId());
+        listing.setStatus(ListingStatus.ACTIVE);
+        listing.setEditToken(Token.generate());
+        listing.setCreatedAt(LocalDateTime.now());
+        listing.setExpiresAt(LocalDateTime.now().plusDays(5));
+
+        return listing;
+    }
+
+    public static ListingResponse mapToCreateListingResponse(){
+        ListingResponse response = new ListingResponse();
+        response.setMesage("Listing created successfully");
+
+        return response;
+    }
+
 }
