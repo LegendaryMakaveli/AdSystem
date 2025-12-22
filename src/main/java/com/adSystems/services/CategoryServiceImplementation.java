f4ca52479b8a8a90873ce087e30743bc6292c9cc
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 21:48:16 +0100

    category service implementation

--- src/main/java/com/adSystems/services/CategoryServiceImplementation.java
+++ src/main/java/com/adSystems/services/CategoryServiceImplementation.java
@@ -1,4 +1,29 @@
 package com.adSystems.services;
 
-public class CategoryServiceImplementation {
+import com.adSystems.datas.models.Category;
+import com.adSystems.datas.repositories.CategoryRepository;
+import com.adSystems.exception.ValidationException;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class CategoryServiceImplementation implements CategoryService{
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
+    @Override
+    public List<Category> getRootCategories() {
+        return categoryRepository.findParentIdIsNull();
+    }
+
+    @Override
+    public List<Category> getSubCategories(String rootCategoryId) {
+        if(rootCategoryId == null || rootCategoryId.trim().isEmpty()) throw new ValidationException("Parent id cannot be empty");
+        return categoryRepository.findByParentId(rootCategoryId);
+    }
 }
