0414eb8aa5970e97043b9483826e46b3643de17e
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 23:20:27 +0100

    APi response

--- src/main/java/com/adSystems/dtos/reponses/APiResponse.java
+++ src/main/java/com/adSystems/dtos/reponses/APiResponse.java
@@ -1,4 +1,13 @@
 package com.adSystems.dtos.reponses;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Data
 public class APiResponse {
+    private boolean isSuccessful;
+    private Object data;
 }
