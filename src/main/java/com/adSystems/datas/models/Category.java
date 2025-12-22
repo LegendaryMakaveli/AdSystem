91dea6f8dafb58f0c5c0566294efac7f4c37e8b1
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 19:26:47 +0100

    update category with database indexes

--- src/main/java/com/adSystems/datas/models/Category.java
+++ src/main/java/com/adSystems/datas/models/Category.java
@@ -3,13 +3,15 @@ package com.adSystems.datas.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "Categorys")
+@Document(collection = "Categories")
 public class Category {
     @Id
     private String id;
     private String name;
+    @Indexed
     private String parentId;
 }
