0b56bddd58bb14fa0d97ce07ccab1bb95381fd89
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 21:47:41 +0100

    category service interfacw

--- src/main/java/com/adSystems/services/CategoryService.java
+++ src/main/java/com/adSystems/services/CategoryService.java
@@ -1,4 +1,10 @@
 package com.adSystems.services;
 
+import com.adSystems.datas.models.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> getRootCategories();
+    List<Category> getSubCategories(String rootCategoryId);
 }
