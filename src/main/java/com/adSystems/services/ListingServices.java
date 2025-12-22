a7e042c0c83f0d54adac77e0679a5f113d0c0b42
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 21:22:31 +0100

    Listing service interface

--- src/main/java/com/adSystems/services/ListingServices.java
+++ src/main/java/com/adSystems/services/ListingServices.java
@@ -1,4 +1,20 @@
 package com.adSystems.services;
 
+import com.adSystems.datas.models.Listing;
+import com.adSystems.dtos.reponses.ListingResponse;
+import com.adSystems.dtos.reponses.UpdateListingResponse;
+import com.adSystems.dtos.requests.GetByCityAndCategoryRequest;
+import com.adSystems.dtos.requests.ListingRequests;
+import com.adSystems.dtos.requests.UpdateListingRequest;
+
+import java.util.List;
+
 public interface ListingServices {
+    ListingResponse createListing(ListingRequests request);
+    Listing getById(String id);
+    List<Listing> getByCity(String cityId);
+    List<Listing> getByCityAndCategory(GetByCityAndCategoryRequest request);
+    UpdateListingResponse updateListing(String id, String token, UpdateListingRequest request);
+    ListingResponse deleteListing(String id, String token);
+    void expireListing();
 }
