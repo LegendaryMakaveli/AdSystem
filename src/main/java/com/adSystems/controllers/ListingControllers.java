f5001c84cb241149932be4420a9e24241ebdc012
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 23:18:47 +0100

    Listing controller done

---
+++ src/main/java/com/adSystems/controllers/ListingControllers.java
@@ -0,0 +1,73 @@
+package com.adSystems.controllers;
+
+import com.adSystems.dtos.reponses.APiResponse;
+import com.adSystems.dtos.requests.ListingRequests;
+import com.adSystems.dtos.requests.UpdateListingRequest;
+import com.adSystems.exception.ClassifiedAdSystemException;
+import com.adSystems.services.ListingServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/listings")
+public class ListingControllers {
+    @Autowired
+    private ListingServices listingServices;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createAnAd(@RequestBody ListingRequests request){
+        try {
+            return new ResponseEntity<>(new APiResponse(true, listingServices.createListing(request)),HttpStatus.CREATED);
+        }catch (ClassifiedAdSystemException error){
+            return new ResponseEntity<>(new APiResponse(false, error.getMessage()),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findAnAdById(@PathVariable("id") String id){
+        try {
+            return new ResponseEntity<>(new APiResponse(true, listingServices.getById(id)),HttpStatus.OK);
+        }catch (ClassifiedAdSystemException error){
+            return new ResponseEntity<>(new APiResponse(false, error.getMessage()),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/city/{cityId}")
+    public ResponseEntity<?> findAnAdByCity(@PathVariable("cityId") String cityId){
+        try {
+            return new ResponseEntity<>(new APiResponse(true, listingServices.getByCity(cityId)),HttpStatus.OK);
+        }catch (ClassifiedAdSystemException error){
+            return new ResponseEntity<>(new APiResponse(false, error.getMessage()),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/city/{cityId}/category/{categoryId}")
+    public ResponseEntity<?> findByCityAndCategory(@PathVariable("cityId")String cityId, @PathVariable("categoryId")String categoryId){
+        try{
+            return new ResponseEntity<>(new APiResponse(true, listingServices.getByCityAndCategory(cityId, categoryId)),HttpStatus.OK);
+        }catch (ClassifiedAdSystemException error){
+            return new ResponseEntity<>(new APiResponse(false, error.getMessage()),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateListing(@PathVariable("id")String id, @RequestParam("token")String token, @RequestBody UpdateListingRequest request ){
+        try{
+            return new ResponseEntity<>(new APiResponse(true, listingServices.updateListing(id,token,request)), HttpStatus.ACCEPTED);
+        }catch (ClassifiedAdSystemException error){
+            return new ResponseEntity<>(new APiResponse(false, error.getMessage()),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteListing(@PathVariable("id")String id, @RequestParam("token")String token){
+        try{
+            return new ResponseEntity<>(new APiResponse(true, listingServices.deleteListing(id,token)), HttpStatus.OK);
+        }catch (ClassifiedAdSystemException error){
+            return new ResponseEntity<>(new APiResponse(false, error.getMessage()),HttpStatus.BAD_REQUEST);
+        }
+    }
+}
