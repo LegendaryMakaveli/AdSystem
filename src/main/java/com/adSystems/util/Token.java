c239c0cbf5af20e25585ba187dc993dde66c658b
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 20:34:24 +0100

    token

--- src/main/java/com/adSystems/util/Token.java
+++ src/main/java/com/adSystems/util/Token.java
@@ -1,4 +1,15 @@
 package com.adSystems.util;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Token {
+
+    public static String generate() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
 }
