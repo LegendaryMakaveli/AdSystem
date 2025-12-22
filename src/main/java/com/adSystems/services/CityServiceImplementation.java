c8be633483f85681d59c1842f95df7f44c96ca7f
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 21:54:19 +0100

    city service iplementatione

--- src/main/java/com/adSystems/services/CityServiceImplementation.java
+++ src/main/java/com/adSystems/services/CityServiceImplementation.java
@@ -1,4 +1,22 @@
 package com.adSystems.services;
 
-public class CityServiceImplementation {
+import com.adSystems.datas.models.City;
+import com.adSystems.datas.repositories.CityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class CityServiceImplementation implements CityService{
+    @Autowired
+    private CityRepository cityRepository;
+
+
+    @Override
+    public List<City> getAllCities() {
+        return cityRepository.findAll();
+    }
 }
