0c861d831fb1cbfdd2499f1ebb62f7d011486dc2
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 19:28:30 +0100

    update listing with database indexes

--- src/main/java/com/adSystems/datas/models/Listing.java
+++ src/main/java/com/adSystems/datas/models/Listing.java
@@ -2,12 +2,25 @@ package com.adSystems.datas.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Data
 @Document(collection = "Listings")
+
+@CompoundIndexes({
+        @CompoundIndex(
+                name = "city_status_createdAt_idx",
+                def = "{'cityId': 1, 'status': 1, 'createdAt': -1}"
+        ),
+        @CompoundIndex(
+                name = "city_category_status_createdAt_idx",
+                def = "{'cityId': 1, 'categoryId': 1, 'status': 1, 'createdAt': -1}"
+        )
+})
 public class Listing {
     @Id
     private String id;
