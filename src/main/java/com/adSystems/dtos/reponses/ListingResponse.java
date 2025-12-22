2ad3b30e69887f8566dce58d13485e10fe9d8d53
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-22 20:32:37 +0100

    app responses

--- src/main/java/com/adSystems/dtos/reponses/ListingResponse.java
+++ src/main/java/com/adSystems/dtos/reponses/ListingResponse.java
@@ -1,4 +1,9 @@
 package com.adSystems.dtos.reponses;
 
+
+import lombok.Data;
+
+@Data
 public class ListingResponse {
+    private  String mesage;
 }
--- src/main/java/com/adSystems/dtos/reponses/UpdateListingResponse.java
+++ src/main/java/com/adSystems/dtos/reponses/UpdateListingResponse.java
@@ -1,4 +1,9 @@
 package com.adSystems.dtos.reponses;
 
+
+import lombok.Data;
+
+@Data
 public class UpdateListingResponse {
+    private String message;
 }
