385a855fd81f391401ac5adac19fc766e53833b0
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2026-01-09 13:31:13 +0100

    updates

--- src/main/java/com/adSystems/config/SecurityConfig.java
+++ src/main/java/com/adSystems/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws ClassifiedAdSystemException {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
